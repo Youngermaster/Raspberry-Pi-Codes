e50eba22d1cea2f850011ee2f0688e79cff7044d
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-05-17 17:01:12 -0500

    Infinite Loop added

--- ZeroW/SDL2/main.cpp
+++ ZeroW/SDL2/main.cpp
@@ -14,28 +14,29 @@ int main(int argc, char* args[]) {
     SDL_Surface* screenSurface = NULL;
 
     // Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-    } else {
-        // Create window
-        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (window == NULL) {
-            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+    while (true) {
+        if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+            printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         } else {
-            // Get window surface
-            screenSurface = SDL_GetWindowSurface(window);
+            // Create window
+            window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+            if (window == NULL) {
+                printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+            } else {
+                // Get window surface
+                screenSurface = SDL_GetWindowSurface(window);
 
-            // Fill the surface white
-            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+                // Fill the surface white
+                SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
 
-            // Update the surface
-            SDL_UpdateWindowSurface(window);
+                // Update the surface
+                SDL_UpdateWindowSurface(window);
 
-            // Wait two seconds
-            SDL_Delay(2000);
+                // Wait two seconds
+                SDL_Delay(2000);
+            }
         }
     }
-
     // Destroy window
     SDL_DestroyWindow(window);
 
