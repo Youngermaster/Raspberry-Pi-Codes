5c4d54e23239464590cb10c1914099a2cf9f6083
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-06-19 19:03:16 -0500

    Cascade classifier

--- ZeroW/OpenCV/GeeksForGeeks/cascade_classifier/cascade_classifier.cpp
+++ ZeroW/OpenCV/GeeksForGeeks/cascade_classifier/cascade_classifier.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/objdetect.hpp"
-#include "opencv2/videoio.hpp"
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/videoio.hpp>
 
 using namespace std;
 using namespace cv;
