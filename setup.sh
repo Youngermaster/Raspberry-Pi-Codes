2e5b57890408f5b2337abf44d2e4c62ef2cfa8dc
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-06-24 22:18:16 -0500

    Button + LED added

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
 sudo apt-get update -y
 sudo apt-get install vim git g++ make cmake build-essential neofetch screenfetch net-tools curl wget -y
+sudo apt-get install python-rpi.gpio python3-rpi.gpio -y
