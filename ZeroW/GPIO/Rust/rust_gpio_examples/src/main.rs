ea0368eb0f1093339543a2b385420dc47f18146d
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-06-26 17:22:00 -0500

    example of Pywebview added

--- ZeroW/GPIO/Rust/rust_gpio_examples/src/main.rs
+++ ZeroW/GPIO/Rust/rust_gpio_examples/src/main.rs
@@ -3,6 +3,10 @@
 use rust_gpiozero::*;
 
 fn main() {
+    // Create a button which is attached to Pin 17
+    // let mut button = Button::new(17);
+    // button.wait_for_press(None);
+    // println!("button pressed");
     // Create a new LED attached to Pin 17
     let mut led = LED::new(17);
 
