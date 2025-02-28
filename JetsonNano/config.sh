d0b21232f8d52656f5569f8ce56bcf9d485eb384
Juan Manuel Young Hoyos <vndrzrfshJXY@wegmans.com>
2025-02-28 00:58:46 -0500

    some jetson nano config stuff

--- JetsonNano/config.sh
+++ JetsonNano/config.sh
@@ -1,13 +1,11 @@
 sudo apt-get update -y
-sudo apt-get install git cmake libpython3-dev python3-pip python3-numpy -y
+sudo apt-get install git cmake libpython3-dev python3-numpy -y
 
-git clone --recursive https://github.com/dusty-nv/jetson-inference.git
+git clone --recursive --depth=1 https://github.com/dusty-nv/jetson-inference
 cd jetson-inference
 mkdir build
 cd build
 cmake ../
-make
+make -j$(nproc)
 sudo make install
-
 sudo ldconfig
-
